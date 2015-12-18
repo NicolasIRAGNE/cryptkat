c48bf4d87e8884cf45569cf4c482271f1fab441d
NicolasIRAGNE <nicoragne@hotmail.fr>
2015-12-19 00:17:33 +0100

    Revert "Revert "kljfoq""
    This reverts commit 1784ad5177fca8a2e46b426b55f41b967d0a026e.

---
+++ icones.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+cleverte = PhotoImage(file='images/cleverte.gif')
+clerouge = PhotoImage(file='images/clerouge.gif')
+rotationImage = PhotoImage(file='images/eng.gif')
+morseImage = PhotoImage(file='images/morse.gif')
+miroirImage = PhotoImage(file="images/miroir.gif")
+quitter = PhotoImage(file='images/quitter.gif')
+sound = PhotoImage(file="images/sound.gif")
