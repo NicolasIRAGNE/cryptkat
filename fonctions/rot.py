7926b9ca35c49deea759e938460ce47e9a642061
NicolasIRAGNE <nicoragne@hotmail.fr>
2015-12-28 17:03:32 +0100

    cesar progressif

--- fonctions/rot.py
+++ fonctions/rot.py
@@ -4,25 +4,14 @@ from fonctions.conversion import *
 def effectuerRotation(messageClair, rotation):
     message =""
     i = 0
-    if rotation < 0: #Si la rotation entrée est négative, elle est multipliée par (-1) et est soustraite plutôt qu'additionnée (dans le cas d'une rotation positive)
-        for i in range(0, len(messageClair)):
-            if messageClair[i].isalpha() == False: #si caractère != lettre, le caractère est ajouté à la chaîne message
-                message += str(messageClair[i])
-                i = i +1
-            else:
-                rotation = -rotation
-                messageCrypte = conversion(messageClair[i]) - rotation
-                messageCrypte = conversion(messageCrypte)
-                message += str(messageCrypte)
-    else:
-        for i in range(0, len(messageClair)):
-            if messageClair[i].isalpha() == False: #si caractère != lettre, le caractère est ajouté à la chaîne message
-                message += str(messageClair[i])
-                i = i +1
-            else:
-                messageCrypte = conversion(messageClair[i]) + rotation
-                messageCrypte = conversion(messageCrypte)
-                message += str(messageCrypte)
+    for i in range(0, len(messageClair)):
+        if messageClair[i].isalpha() == False: #si caractère != lettre, le caractère est ajouté à la chaîne message
+            message += str(messageClair[i])
+            i = i +1
+        else:
+            messageCrypte = conversion(messageClair[i]) + rotation
+            messageCrypte = conversion(messageCrypte)
+            message += str(messageCrypte)
     return(message)
 
 
--- interface/boutons.py
+++ interface/boutons.py
@@ -4,6 +4,7 @@ from interface.icones import *
 from fonctions.conversion import *
 from fonctions.atbash import *
 from fonctions.rot import *
+from fonctions.progressif import *
 import fonctions.morse
 import fonctions.vigenere
 import fonctions.bruteforce
@@ -43,23 +44,36 @@ def fonction_bouton_miroir():
     champResultat.delete(0, END)
     champResultat.insert(0,(miroir(str(saisieMessage.get().lower()))))
 
+def fonction_bouton_cesar_progressif():
+    champResultat.delete(0, END)
+    #champResultat.insert(0,(cesar(str(saisieMessage.get().lower(), int(saisieRotation.get()%26, int(champPas.get()))))))
+    champResultat.insert(0,cesar(str(saisieMessage.get().lower()), int(saisieRotation.get())%26, int(champPas.get())))
+    #saisieMessage.get().lower()
+    #saisieRotation.get()
+    #champPas.get()
+
+
 
 
 labelSaisieMessage = Label(fen, text="Entrez votre message ici.")
 saisieMessage = Entry(fen, width=40)
 
-labelSaisieCle = Label(fen, text="Entrez votre clé ici")
+labelSaisieCle = Label(fen, text="Entrez votre clÃ© ici")
 saisieCle = Entry(fen)
 
 boutonCrypterVigenere = Button(fen,compound=LEFT,image=clerouge, text='Vigenere (Crypter)    ', width =150, command=fonction_bouton_vigenere_crypter)
 boutonDecrypterVigenere = Button(fen,text='Vigenere (Decrypter)',compound=LEFT,image=cleverte, width =150, command=fonction_bouton_vigenere_decrypter)
 
 boutonRotation = Button(fen,compound=LEFT,image=rotationImage, text="        Rotation              ", width = 150, command=fonction_bouton_rotation)
-labelRotation = Label(fen, text="Rotation désirée:")
+labelRotation = Label(fen, text="Rotation dÃ©sirÃ©e:")
 labelRotation2 = Label(fen, text="(nombre entier)")
 saisieRotation = Entry(fen)
 
 boutonRotationBruteforce = Button(fen,compound=LEFT,image=bruteforceImage, text="Bruteforce Rotation  ", width = 150, command=fonction_bouton_rotation_bruteforce)
+
+boutonCesarProgressif = Button(fen, compound= LEFT, image=bruteforceImage, text = "Cesar progressif", width=150, command = fonction_bouton_cesar_progressif)
+labelPas = Label(fen, text="Pas:")
+champPas = Entry(fen)
 
 boutonMiroir = Button(fen,compound=LEFT,image=miroirImage, text="   Miroir (Atbash)      ", width = 150, command=fonction_bouton_miroir)
 
@@ -67,7 +81,7 @@ boutonMorseCrypter = Button(fen, text="Morse (Crypter)",compound=LEFT,image=mors
 boutonMorseDecrypter = Button(fen, text="Morse (Decrypter)",compound=LEFT,image=morseImage2, width = 150, command=fonction_bouton_morse_decrypter)
 boutonMorseAudio = Button(fen, image=sound, width = 50, command=fonction_bouton_morse_audio)
 
-labelResultat = Label(fen, text="Résultat:")
+labelResultat = Label(fen, text="RÃ©sultat:")
 champResultat = Entry(fen, width=40)
 
 boutonQuitter = Button(fen,text='Quitter',compound=LEFT,image=quitter, width =150, command=fen.destroy)
