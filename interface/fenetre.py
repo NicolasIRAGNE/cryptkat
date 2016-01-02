a22287b18da1bbc9538e9ffd5ef80fe5ed02b77c
NicolasIRAGNE <nicoragne@hotmail.fr>
2016-01-02 02:13:05 +0100

    code plus joli

--- interface/fenetre.py
+++ interface/fenetre.py
@@ -3,5 +3,16 @@ from random import choice
 fen = Tk()
 fen.title("PROJET 3.8")
 fen.resizable(0,0)
-messagesPossibles = ["Bonjour !", "Salut !", "Hey !","Master of puppets I'm pulling your strings", "Ca fait 3 mois Antoine", "Ceci est un message d'exemple", "Coucou", "Les dataries feront l'affaire", "Les framboises sont perchées sur le tabouret de mon grand père", "C'est pas faux", ".... . -.--", "Bonjour !", "Yoooooo"]
+messagesPossibles = ["Bonjour !",
+"Salut !", 
+"Hey !",
+"Master of puppets I'm pulling your strings",
+"Ca fait 3 mois Antoine",
+"Ceci est un message d'exemple",
+"Coucou", "Les dataries feront l'affaire",
+"Les framboises sont perchées sur le tabouret de mon grand père",
+"C'est pas faux",
+".... . -.--",
+"Bonjour !", 
+"Yoooooo"]
 messageAccueil = choice(messagesPossibles)
